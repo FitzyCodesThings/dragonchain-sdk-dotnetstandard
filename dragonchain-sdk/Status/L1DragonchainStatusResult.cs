cf82954b6ab28e4cbcfb99a467672545291dd5aa
John "Fitzy" DeLancey <fitzy@fct.dev>
2020-07-24 13:20:18 -0500

    Cleaned up L1DragonchainStatusResult model (removed unused properties, added missing properties).

--- dragonchain-sdk/Status/L1DragonchainStatusResult.cs
+++ dragonchain-sdk/Status/L1DragonchainStatusResult.cs
@@ -3,29 +3,21 @@
     public class L1DragonchainStatusResult
     {
         /// <summary>
+        /// Public ID of the Dragonchain
+        /// </summary>
+        public string Id { get; set; }
+        /// <summary>
         /// Level of this dragonchain (as an integer)
         /// </summary>
-        public string Level { get; set; }
-        /// <summary>
-        /// Cloud that this chain is running in
-        /// </summary>
-        public string Cloud { get; set; }
+        public string Level { get; set; }        
         /// <summary>
         /// URL of the chain
         /// </summary>
-        public string Url { get; set; }
-        /// <summary>
-        /// Region that this chain is operating in
-        /// </summary>
-        public string Region { get; set; }
+        public string Url { get; set; }        
         /// <summary>
         /// Proof scheme that this chain uses
         /// </summary>
-        public string Scheme { get; set; }
-        /// <summary>
-        /// Ethereum wallet assigned to this chain
-        /// </summary>
-        public string Wallet { get; set; }
+        public string Scheme { get; set; }        
         /// <summary>
         /// Hashing algorithm used for blocks on this chain
         /// </summary>
@@ -38,5 +30,9 @@
         /// Encryption algorithm used for blocks on this chain
         /// </summary>
         public string EncryptionAlgo { get; set; }
+        /// <summary>
+        /// Indicates whether indexing has been enabled for this chain
+        /// </summary>
+        public bool IndexingEnabled { get; set; }
     }
 }
